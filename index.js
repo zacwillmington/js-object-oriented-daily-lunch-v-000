b15376d3825b84b29c207d5962c37b8f70fe9093
Zac Willmington <zac.willmington@gmail.com>
2018-09-15 16:15:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -79,9 +79,10 @@ class Meal {
     }
 
     static byPrice() {
-        return store.meals.sort(function(a, b){
+        const p = store.meals.sort(function(a, b){
             a.price - b.price;
         });
+        console.log(p);
     }
 }
 
