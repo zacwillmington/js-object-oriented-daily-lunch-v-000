f3bad1abfb233ed5c9d0e99716b0e3ba1f495ece
Zac Willmington <zac.willmington@gmail.com>
2018-09-15 16:05:02 +0000

    automatically backed up by learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -192,6 +192,7 @@ describe('index.js', () => {
 
       describe('meals()', () => {
         it('returns all unique meals a customer has ordered', () => {
+            debugger;
           expect(guy.meals()).to.deep.equal([friedCheesecake, macAndCheese, macAndCheese]);
         });
       });
